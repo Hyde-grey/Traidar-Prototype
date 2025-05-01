078431404047e8ae2583d516edb1f2154c068884
Hyde Francois Khamsing <francoiskhamsing@gmail.com>
2025-05-01 15:19:02 +0100

    feat: add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+
+import TraidarLogo from "../src/IMG/Traidar_Logo_white.png"
 import "./App.css";
 
 function App() {
@@ -5,7 +7,9 @@ function App() {
     <>
       <div className="main-container">
         <div className="navigation-bar">
-          <div className="logo"></div>
+          <div className="logo">
+            <img src={TraidarLogo} alt="Traidar-logo"/>
+          </div>
         </div>
       </div>
     </>
