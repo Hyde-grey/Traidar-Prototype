15672e74e90652af54d6610bcf19e6157d3653b2
Hyde <francoiskhamsing@gmail.com>
2025-05-01 13:28:10 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="navigation-bar">
+          <div className="logo"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
