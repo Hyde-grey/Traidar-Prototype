9f2c5bdb877523a5c6f5034182e7f5fe87310330
Hyde <francoiskhamsing@gmail.com>
2025-05-13 01:49:43 +0100

    fix:deployment issues

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ import App from "./App.tsx";
 import { Amplify } from "aws-amplify";
 import config from "../amplify_outputs.json";
 
-// Configure Amplify
-Amplify.configure(config);
+// Configure Amplify with proper type assertions to prevent TS errors
+Amplify.configure(config as any);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
